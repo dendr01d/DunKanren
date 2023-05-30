734c116bb868617224eae9aa298c1f8e32cb8344
Dendroid <duncanfmiller@gmail.com>
2023-05-29 23:51:27 -0400

    blah

---
+++ DunKanren/Goals/AtomicGoal.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DunKanren.Goals
+{
+    /// <summary>
+    /// An atomic goal is one that performs a trivial operation on strictly terms.
+    /// Namely top/bottom, unification, disunification, or type-checking.
+    /// The truly important thing is that these goals are all trivially negatable
+    /// </summary>
+    public abstract class AtomicGoal : SimpleGoal
+    {
+        protected AtomicGoal(Func<State, Stream> app) : base(app) { }
+
+    }
+
+    #region arbitrary True/False goals
+
+    public class Top : AtomicGoal
+    {
+        public Top() : base(s => Stream.Singleton(s)) { }
+        public override AtomicGoal Negate() => new Bottom();
+    }
+
+    public class Bottom : AtomicGoal
+    {
+        public Bottom() : base(s => Stream.Empty()) { }
+        public override AtomicGoal Negate() => new Top();
+    }
+
+    #endregion
+
+    public abstract class UnaryAtomicGoal : AtomicGoal
+    {
+        protected Term Operand;
+
+        protected UnaryAtomicGoal(Term operand, Func<State, bool> app, string op, string longOp) :
+            base (s => Stream.PassThrough(s, app(s)))
+        {
+            this.Operand = operand;
+            this.Expression = "(" + operand.ToString() + " " + op + ")";
+            this.Description = "The following term is " + longOp + ":";
+            this.Children = new List<IPrintable>() { operand };
+        }
+    }
+
+
+    #region Type-checking
+
+    public class Symbolo : UnaryAtomicGoal
+    {
+        public Symbolo(Term t) : base(t, s => t.IsSymbol(s), "is SYMBOL", "a Symbol object") { }
+        public override AtomicGoal Negate() => new NotSymbolo(this.Operand);
+    }
+
+    public class NotSymbolo : UnaryAtomicGoal
+    {
+        public NotSymbolo(Term t) : base(t, s => !t.IsSymbol(s), "not SYMBOL", "not a Symbol object") { }
+        public override AtomicGoal Negate() => new Symbolo(this.Operand);
+    }
+
+    public class Numbero : UnaryAtomicGoal
+    {
+        public Numbero(Term t) : base(t, s => !t.IsNumber(s), "is NUMBER", "a Number object") { }
+        public override AtomicGoal Negate() => new NotNumbero(this.Operand);
+    }
+
+    public class NotNumbero : UnaryAtomicGoal
+    {
+        public NotNumbero(Term t) : base(t, s => !t.IsNumber(s), "not NUMBER", "not a Number object") { }
+        public override AtomicGoal Negate() => new Numbero(this.Operand);
+    }
+
+    public class Emptyo : UnaryAtomicGoal
+    {
+        public Emptyo(Term t) : base(t, s => t.IsEmpty(s), "is NIL", "a Nil object") { }
+        public override AtomicGoal Negate() => new NotEmptyo(this.Operand);
+    }
+
+    public class NotEmptyo : UnaryAtomicGoal
+    {
+        public NotEmptyo(Term t) : base(t, s => !t.IsEmpty(s), "not NIL", "not a Nil object") { }
+        public override AtomicGoal Negate() => new Emptyo(this.Operand);
+    }
+
+    public class Pairo : UnaryAtomicGoal
+    {
+        public Pairo(Term t) : base(t, s => t.IsPair(s), "is PAIR", "a Pair object") { }
+        public override AtomicGoal Negate() => new NotPairo(this.Operand);
+    }
+
+    public class NotPairo : UnaryAtomicGoal
+    {
+        public NotPairo(Term t) : base(t, s => t.IsPair(s), "not PAIR", "not a Pair object") { }
+        public override AtomicGoal Negate() => new Pairo(this.Operand);
+    }
+
+    #endregion
+
+    public abstract class BinaryAtomicGoal : AtomicGoal
+    {
+        protected Term LeftOperand;
+        protected Term RightOperand;
+
+        protected BinaryAtomicGoal(Term left, Term right, Func<State, State?> app, string op, string longOp) :
+            base(s => Stream.Singleton(app(s)))
+        {
+            this.LeftOperand = left;
+            this.RightOperand = right;
+            this.Expression = "(" + left.ToString() + " " + op + " " + right.ToString() + ")";
+            this.Description = "The following terms are " + longOp + ":";
+            this.Children = new List<IPrintable>() { left, right };
+        }
+    }
+
+    public class Equality : BinaryAtomicGoal
+    {
+        public Equality(Term left, Term right) :
+            base(left, right, s => s.Unify(left, right), "≡", "equivalent")
+        { }
+
+        public override AtomicGoal Negate() => new Disequality(this.LeftOperand, this.RightOperand);
+    }
+
+    public class Disequality : BinaryAtomicGoal
+    {
+        public Disequality(Term left, Term right) :
+            base(left, right, s => s.DisUnify(left, right), "╪", "NOT equivalent")
+        { }
+
+        public override AtomicGoal Negate() => new Equality(this.LeftOperand, this.RightOperand);
+    }
+}
---
+++ DunKanren/Goals/Goal.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DunKanren.Goals
+{
+    /// <summary>
+    /// AKA "Hierarchical" Goal
+    /// </summary>
+    public abstract partial class Goal : IPrintable
+    {
+        private int PersonalID;
+        private static int GoalCounter = 0;
+
+        public virtual string Expression { get; protected set; }
+        public virtual string Description { get; protected set; }
+
+        protected Func<State, Stream> Application;
+
+        protected List<IPrintable> Children;
+
+        protected Goal()
+        {
+            this.PersonalID = ++GoalCounter;
+            this.Expression = "Unknown Goal";
+            this.Description = "Application of Unknown Goal";
+            this.Application = (State s) => throw new NotImplementedException("Goal was assigned no application function");
+            this.Children = new List<IPrintable>();
+        }
+
+        protected Goal(Func<State, Stream> fun) : this()
+        {
+            this.Application = fun;
+        }
+
+        public Stream PursueIn(State s)
+        {
+            return this.Application(s);
+        }
+
+        public Stream Pursue()
+        {
+            return this.PursueIn(State.InitialState());
+        }
+
+        protected static string[] ExtractVarNames(System.Reflection.MethodInfo funInfo)
+        {
+            char defaultSymbol = 'A';
+            return funInfo.GetParameters().Select(x => x.Name ?? "v" + (defaultSymbol++).ToString()).ToArray();
+        }
+
+        public string GetName() => "Goal #" + this.PersonalID + ": " + this.ToString();
+
+        public override string ToString() => this.Expression;
+        public virtual string ToVerboseString() => this.Description;
+
+        public IEnumerable<string> ToTree() => this.ToTree("", true, false);
+        public IEnumerable<string> ToTree(string prefix, bool first, bool last)
+        {
+            string parentPrefix = first ? "" : prefix + (last ? IO.LEAVES : IO.BRANCH);
+            string childPrefix = first ? "" : prefix + (last ? IO.SPACER : IO.JUMPER);
+
+
+            //yield return parentPrefix + IO.HEADER + g.Description;
+
+            using (var iter = this.Children.GetEnumerator())
+            {
+                bool move1 = iter.MoveNext();
+
+                //if this goal won't print any children then 
+                yield return parentPrefix + (move1 ? IO.HEADER : IO.ALONER) + this.Description;
+
+                IPrintable sub = iter.Current;
+
+                while (sub != null)
+                {
+                    if (iter.MoveNext())
+                    {
+                        foreach (string line in sub.ToTree(childPrefix, false, false))
+                        {
+                            yield return line;
+                        }
+                    }
+                    else
+                    {
+                        foreach (string line in sub.ToTree(childPrefix, false, true))
+                        {
+                            yield return line;
+                        }
+                        //yield return prefix + IO.JUMPER;
+                    }
+
+                    sub = iter.Current;
+                }
+            }
+        }
+    }
+
+    public class FutureGoal : Goal
+    {
+        public FutureGoal() : base()
+        {
+            this.Expression = "{Unevaluated Goal}";
+            this.Description = "{Application of Unevaluated Goal}";
+        }
+    }
+}
